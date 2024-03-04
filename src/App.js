d91c9b9a5d76aa7f2ee11d2c732e64d821f59c65
nrm00014 <nrm00014@mix.wvu.edu>
2024-03-03 21:33:45 -0500

    Week 8

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import './Card.js';
+import '.Navbar.js';
 
 function App() {
   return (
     <div className="App">
+    <nav class = "navbar"></nav>
     <h1>CS 230L</h1>
     <h2>Section-002</h2>
     <p>WVU ID: 800373566</p>
     <p>Hi I am Nathan Mauzy</p>
+    <div class = "card"></div>
     </div>
   );
 }
